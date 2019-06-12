95afd1acdb77b81cff6e33b24584100f6ebcc590
Alex <komarevich.alex@gmail.com>
2019-06-13 00:58:10 +0300

    footer add

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="Footer">
-      
+    <footer className="footer">
+      <div className="footer-social">
+        <a href="#" target="_blank">
+          <i class="fab fa-facebook-f" />
+        </a>
+        <a href="#" target="_blank">
+          <i class="fab fa-instagram" />
+        </a>
+        <a href="#" target="_blank">
+          <i class="fab fa-github" />
+        </a>
+        <a href="#" target="_blank">
+          <i class="fab fa-twitter" />
+        </a>
+      </div>
+      <span className="copyright">By Alexander Komarevich ©2019</span>
     </footer>
-  )
+  );
 }
 export default Footer
