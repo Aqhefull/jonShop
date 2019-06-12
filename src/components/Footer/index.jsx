f31527d6fac79c058c40f43438b9785c56764303
Alex <komarevich.alex@gmail.com>
2019-06-13 01:06:28 +0300

    footer fixup 2

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,16 +4,16 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-social">
-        <a href="#" target="_blank">
+        <a href="https://facebook.com" rel="noopener noreferrer" target="_blank">
           <i class="fab fa-facebook-f" />
         </a>
-        <a href="#" target="_blank">
+        <a href="https://instagram.com" rel="noopener noreferrer" target="_blank">
           <i class="fab fa-instagram" />
         </a>
-        <a href="#" target="_blank">
+        <a href="https://github.com" rel="noopener noreferrer" target="_blank">
           <i class="fab fa-github" />
         </a>
-        <a href="#" target="_blank">
+        <a href="https://twitter.com" rel="noopener noreferrer" target="_blank">
           <i class="fab fa-twitter" />
         </a>
       </div>
