33324cf08bba516f736672e5ac247044b6135e36
Alex <komarevich.alex@gmail.com>
2019-06-17 21:17:34 +0300

    Updates

---
+++ precache-manifest.676da7c0895b022d6d2de250c9202ec4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "74c862f664b205999e8c95cfc63d2c4d",
+    "url": "/jonShop/static/media/off.74c862f6.svg"
+  },
+  {
+    "revision": "2a3c912bc9e010845cc9694691b16968",
+    "url": "/jonShop/static/media/cart.2a3c912b.svg"
+  },
+  {
+    "revision": "803c6c03da9224c8f2362aca5efeb48c",
+    "url": "/jonShop/static/media/arrow.803c6c03.svg"
+  },
+  {
+    "revision": "3f5ae1c1e0d9c9527dd0c3b0ff97f5d4",
+    "url": "/jonShop/static/media/arrow-white.3f5ae1c1.svg"
+  },
+  {
+    "revision": "6f2ee63ffd4a7336397a",
+    "url": "/jonShop/static/js/runtime~main.3ef45f09.js"
+  },
+  {
+    "revision": "ca0f2c4b04ad1e24ff67",
+    "url": "/jonShop/static/js/main.8dc1d329.chunk.js"
+  },
+  {
+    "revision": "c63739cc5e736b9217cc",
+    "url": "/jonShop/static/js/2.aab0d9e6.chunk.js"
+  },
+  {
+    "revision": "ca0f2c4b04ad1e24ff67",
+    "url": "/jonShop/static/css/main.f98f6c81.chunk.css"
+  },
+  {
+    "revision": "1044d5b20c55aeec8710129dbb70abb4",
+    "url": "/jonShop/index.html"
+  }
+];
